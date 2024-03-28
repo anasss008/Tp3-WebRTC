f65a45d6849379aac7c28ae02b9929186a91b7a3
an-ls525 <135461496+an-ls525@users.noreply.github.com>
2024-03-28 20:27:36 +0000

    modification du code signalisation/js et resolution du probleme socket.io

--- Signalisation/js/NodeServer.js
+++ Signalisation/js/NodeServer.js
@@ -7,35 +7,70 @@ var app = http
     file.serve(req, res);
   })
   .listen(8181);
-var io = require('socket.io')(app);
+var io = require('socket.io')(app, {
+  cors: {
+    origin: '*',
+    methods: ['GET', 'POST'],
+  },
+});
 io.sockets.on('connection', function (socket) {
   socket.on('message', function (message) {
     log('S --> Got message: ', message);
     socket.broadcast.to(message.channel).emit('message', message.message);
   });
+  // socket.on('create or join', function (channel) {
+  //   var numClients = io.sockets.clients(channel).length;
+  //   console.log('numclients = ' + numClients);
+  //   if (numClients == 0) {
+  //     socket.join(channel);
+  //     socket.emit('created', channel);
+  //   } else if (numClients == 1) {
+  //     io.sockets.in(channel).emit('remotePeerJoining', channel);
+  //     socket.join(channel);
+  //     socket.broadcast
+  //       .to(channel)
+  //       .emit(
+  //         'broadcast: joined',
+  //         'S --> broadcast(): client ' +
+  //           socket.id +
+  //           ' joined channel ' +
+  //           channel
+  //       );
+  //   } else {
+  //     console.log('Channel full!');
+  //     socket.emit('full', channel);
+  //   }
+  // });
   socket.on('create or join', function (channel) {
-    var numClients = io.sockets.clients(channel).length;
-    console.log('numclients = ' + numClients);
-    if (numClients == 0) {
-      socket.join(channel);
-      socket.emit('created', channel);
-    } else if (numClients == 1) {
-      io.sockets.in(channel).emit('remotePeerJoining', channel);
-      socket.join(channel);
-      socket.broadcast
-        .to(channel)
-        .emit(
-          'broadcast: joined',
-          'S --> broadcast(): client ' +
-            socket.id +
-            ' joined channel ' +
-            channel
-        );
-    } else {
-      console.log('Channel full!');
-      socket.emit('full', channel);
-    }
+    io.in(channel)
+      .allSockets()
+      .then((sockets) => {
+        var numClients = sockets.size;
+        console.log('numclients = ' + numClients);
+
+        if (numClients === 0) {
+          socket.join(channel);
+          socket.emit('created', channel);
+        } else if (numClients === 1) {
+          io.sockets.in(channel).emit('remotePeerJoining', channel);
+          socket.join(channel);
+          socket.broadcast
+            .to(channel)
+            .emit(
+              'broadcast: joined',
+              'S --> broadcast(): client ' +
+                socket.id +
+                ' joined channel ' +
+                channel
+            );
+        } else {
+          // max two clients
+          socket.emit('full', channel);
+        }
+      })
+      .catch((err) => console.error(err));
   });
+
   socket.on('response', function (response) {
     log('S --> Got response: ', response);
     socket.broadcast.to(response.channel).emit('response', response.message);
